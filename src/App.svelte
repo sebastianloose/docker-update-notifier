dfe2713368a568ab1c1d247ee79a7dd4d18f123f
Sebastian Loose <sebastian.loose@yahoo.de>
2021-10-07 12:07:51 +0200

    Migrated to svelte

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	  import { Router, Route } from "svelte-routing";
+	  import Home from "./routes/Home.svelte";
+</script>
+
+<div class="max-w-6xl m-auto p-8">
+	<Router>
+		<Route path="/" component={Home}/>
+	</Router>
+</div>
+
+<style lang="postcss" global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
