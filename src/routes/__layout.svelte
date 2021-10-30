c1142ff5ccc08948d07df129cdc513e2903fac6d
Sebastian Loose <sebastian.loose@yahoo.de>
2021-10-30 18:55:48 +0200

    Minor

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,10 +2,6 @@
     import "../app.css";
 </script>
 
-<svelte:head>
-    <title>Docker Update Notifier</title>
-</svelte:head>
-
 <div class="max-w-6xl m-auto px-5 py-8 sm:p-8">
     <slot />
 </div>
